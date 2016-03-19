b7a1a825b14a11522ac90db53612bd333244e0ca
Michal Malik <michalmalik@wp.pl>
2016-03-19 13:54:29 +0100

    added travis

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Fri Oct 16 2015 17:59:29 GMT+0200 (CEST)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: "",
@@ -48,8 +48,21 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ["Chrome"],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
+  };
+
+  if (process.env.TRAVIS) {
+    configuration.browsers = ["Chrome_travis_ci"];
+  }
+
+  config.set(configuration);
 }
