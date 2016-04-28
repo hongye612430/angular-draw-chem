f42d8146dbe4e165060c024a1f00f7f098035678
Michal Malik <michalmalik@wp.pl>
2016-04-28 02:21:48 +0200

    update

--- javascripts/app-concat.js
+++ javascripts/app-concat.js
@@ -93,18 +93,28 @@
 		vm.svgs = {
 			"Ring-closing metathesis": {
 				file: "metathesis",
-				style: { width: "50%" },
+				style: { width: "750px" },
 				paper: "Malik, M.; Witkowski, G.; Ceborska, M.; Jarosz, S. <i>Org. Lett.</i> <strong>2013</strong>, <i>15</i>, 6214-6217"
 			},
 			"White's macrolactonization": {
 				file: "macrolactonization",
-				style: { width: "65%" },
-				paper: "Fraunhoffer, K.F.; Prabagaran, N.; Sirois, L.E.; White, M. C. <i>JACS</i> <strong>2006</strong>, <i>128</i>, 9032-9033"
+				style: { width: "750px" },
+				paper: "Fraunhoffer, K.F.; Prabagaran, N.; Sirois, L.E.; White, M. C. <i>J. Am. Chem. Soc.</i> <strong>2006</strong>, <i>128</i>, 9032-9033"
 			},
 			"Barton–McCombie deoxygenation": {
 				file: "barton",
-				style: { width: "75%" },
+				style: { width: "750px" },
 				paper: "Tormo, J.; Fu, G. C. <i>Org. Syn.</i> <strong>2002</strong>, <i>78</i>, 239"
+			},
+			"RCM of terminal alkynes": {
+				file: "RCM-alkynes",
+				style: { width: "750px" },
+				paper: "Schulz S. and co-workers <i>Org. Lett.</i> <strong>2015</strong>, <i>17</i>, 5004-5007"
+			},
+			"Organocatalysis": {
+				file: "organocat",
+				style: { width: "750px" },
+				paper: "Kumar, I. and co-workers <i>Org. Lett.</i> <strong>2015</strong>, <i>17</i>, 5582-5585"
 			}
 		};
 		vm.releases = [];
